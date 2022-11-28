e4c9ed7f3bd435402a0dec39031b4fa174c11e8d
DonIsHere46 <117902007+DonIsHere46@users.noreply.github.com>
2022-11-28 12:40:12 +0100

    Changes

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hola Mundo!")
+let fs = require('fs');
+
+fs.readFile('DATA','utf8',function(err, contents){
+    console.log(contents);
+});
+
+console.log('Despues de llamar readFile');
